0d21ca16003042a500c1389038c0862c31e14ff6
cindy <Cdiaz@appdev.yearup.org>
2023-06-08 09:55:12 -0400

    added hyperlink and now working on details page

---
+++ Scripts/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const coursetablecard = document.getElementById("tablecard");
+const coursetablebody = document.getElementById("coursetablebody");
+
+window.onload = function () {
+
+    getAllCoursesFromAPI();
+
+}
+
+function getAllCoursesFromAPI() {
+
+    let theUrl = "http://localhost:8081/api/courses";
+    fetch(theUrl)
+        .then(response => response.json())
+        .then(data => {
+            let coursesArray = data;
+            console.log(data); // all courses, array of objects
+            populateCourseTable(coursesArray);
+        })
+}
+function populateCourseTable(coursesArray) {
+
+    for (let course of coursesArray) {
+        let newRow = coursetablebody.insertRow(-1);
+        let idCell = newRow.insertCell(0);
+        idCell.innerHTML = course.id;
+
+        let deptCell = newRow.insertCell(1);
+        deptCell.innerHTML = course.dept;
+
+        let courseNumCell = newRow.insertCell(2);
+        courseNumCell.innerHTML = course.courseNum;
+
+        let courseNameCell = newRow.insertCell(3);
+        let anchor = document.createElement("a");
+        anchor.href = `details.html?courseid=${course.id}`;
+         anchor.text = course.courseName;
+         courseNameCell.appendChild(anchor);
+
+
+    }
+    console.log('All courses are in the table');
+
+}
+
+
